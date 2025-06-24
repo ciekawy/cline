d9d1397ee0a487d8b4069d216cbe72367eebc703
canvrno <kevin@cline.bot>
2025-06-24 13:01:43 -0700

    Better init handling

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -219,6 +219,7 @@ export class Task {
 				updateTaskHistory: this.updateTaskHistory,
 				say: this.say.bind(this),
 				messageStateHandler: this.messageStateHandler,
+				cancelTask: this.cancelTask,
 			},
 			{
 				conversationHistoryDeletedRange: this.taskState.conversationHistoryDeletedRange,
@@ -366,9 +367,6 @@ export class Task {
 			this.taskState.checkpointTrackerErrorMessage = this.checkpointManager.getCurrentState().checkpointTrackerErrorMessage
 			await this.postStateToWebview()
 		}
-
-		// Cancel and reinitialize the task to get updated messages
-		this.cancelTask()
 	}
 
 	// Communicate with webview
@@ -879,7 +877,7 @@ export class Task {
 		this.mcpHub.clearNotificationCallback()
 	}
 
-	// Checkpoints
+	// Checkpoints logic moved to checkpointManager
 
 	async saveCheckpoint(isAttemptCompletionMessage: boolean = false) {
 		await this.checkpointManager.saveCheckpoint(isAttemptCompletionMessage)
