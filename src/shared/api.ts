dd7c77828abd76bca39663d46c0176e90b5bd315
Szymon Stasik <ciekawy@gmail.com>
2025-10-28 11:36:20 +0100

    added gpt-5-codex

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1420,6 +1420,15 @@ export const geminiCliModels = {
 export type OpenAiNativeModelId = keyof typeof openAiNativeModels
 export const openAiNativeDefaultModelId: OpenAiNativeModelId = "gpt-5-2025-08-07"
 export const openAiNativeModels = {
+	"gpt-5-codex": {
+		maxTokens: 32_768, // 128000 breaks context window truncation
+		contextWindow: 400_000,
+		supportsImages: true,
+		supportsPromptCache: true,
+		inputPrice: 1.25,
+		outputPrice: 10.0,
+		cacheReadsPrice: 0.125,
+	},
 	"gpt-5-2025-08-07": {
 		maxTokens: 8_192, // 128000 breaks context window truncation
 		contextWindow: 272000,
